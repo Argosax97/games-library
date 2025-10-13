cc38b6f346ac4e7dfc84aa6781bbddc33928d78c
Argosax97 <alex.hakobyan.1@gmail.com>
2025-10-13 12:40:58 +0400

    initial setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,7 +11,57 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.plugins(
+    'simple-import-sort',
+    'eslint-plugin-import',
+    'eslint-plugin-prettier',
+  ),
+  ...compat.extends(
+    'next/core-web-vitals',
+    'next/typescript',
+    'prettier',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ),
+  ...compat.config({
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    overrides: [
+      {
+        files: ['**/*.{ts,tsx}'],
+        rules: {
+          'simple-import-sort/imports': [
+            'error',
+            {
+              groups: [
+                ['^react', '^@?\\w'], // Packages, React first
+                ['^(@|lib|components|assets|pages|actions|app)(/.*|$)'], // Internal packages
+                ['^\\u0000'], // Side effect imports
+                ['^\\.\\.(?!/?$)', '^\\.\\./?$'], // Parent imports, `..` last
+                ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'], // Other relative imports
+                ['^.+\\.?(css)$'], // Style imports
+              ],
+            },
+          ],
+        },
+      },
+    ],
+  }),
   {
     ignores: [
       "node_modules/**",
